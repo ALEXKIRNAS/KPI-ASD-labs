8b64b692466894265cee2a4100ca3b779734a0d8
ALEXKIRNAS <alexkirnas@ukr.net>
2016-03-11 14:38:12 +0200

    Add function that printing a head of program.

--- ASD-lab5/ASD-lab5/ASD-lab5.cpp
+++ ASD-lab5/ASD-lab5/ASD-lab5.cpp
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
+void intorduction(void);
+
 int main(void)
 {
 
+}
+
+
+// Printing head of program (name, number)
+void introduction(void)
+{
+	printf("Algorithms and Data Structures\n");
+	printf("Labwork #5\n");
+	printf("Author: Sasha Zarichkovyi\n");
+	printf("Variant: %d\n\n", 5106%21 + 1);
 }
