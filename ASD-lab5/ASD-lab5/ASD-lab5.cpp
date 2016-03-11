23a2aa5442808aa0374c5c5c480769fecbdeb76a
ALEXKIRNAS <alexkirnas@ukr.net>
2016-03-11 14:58:53 +0200

    Adding itegration function.

--- ASD-lab5/ASD-lab5/ASD-lab5.cpp
+++ ASD-lab5/ASD-lab5/ASD-lab5.cpp
@@ -1,11 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+const double calcPerSec = 1e7; // Count of elementary operation per second perform by computer
 
 void introduction(void);
 void menu(void);
+double integration(double(*) (double), double, double);
 
 int main(void)
 {
-	introduction();
+	while (true)
+	{
+		introduction();
+		menu();
+
+		char ch = _getch();
+		switch(ch)
+		{
+		case '1' : 
+		case '2' :
+		case 'q' :
+		case 'Q' : return 0;
+		}
+	}
 }
 
 
@@ -23,4 +41,24 @@ void menu(void)
 {
 	printf("Make your choise.\n");
 	printf("Menu:\n1 : Integrate sh(x)\n2 : Integrate cos(x)\nQ : Exit\n");
+}
+
+/************************
+Integration of <function>
+for <left> to <right>
+*************************/
+double integration(double(*function) (double), double left, double right)
+{
+	double long step = (right - left) / calcPerSec; // Chosing a step of integration
+
+	double integrationResult = 0.0;
+
+	while (left < right)
+	{
+		// Calculation of square figure using by Trapezoidal rule
+		integrationResult += (function(left) + function(left + step)) / 2 * step;
+		left += step;
+	}
+
+	return integrationResult;
 }
